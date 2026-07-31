4dffc2a7fa75e8ec508fb70b7d84a658303c554a
GouthamTuzz <shootwayanad@gmail.com>
2026-07-31 12:03:53 +0530

    Add compiled frontend assets for hosting deployment

---
+++ public/build/assets/challengeToken-DjrKzTWC.js
@@ -0,0 +1,1 @@
+function n(e){return e?.challenge?.token??e?.token??e?.challenge_link?.token??e?.challenge_token??null}function l(e){return`${window.location.origin}/challenge/${e}`}export{l as b,n as e};
